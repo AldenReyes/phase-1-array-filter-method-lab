f5f42766419e5418dd9c85252973c3e0805b85db
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-31 01:58:05 -0700

    slight revision for CodeGrade compatibility

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ findMatching = (driversArray, driversString) => {
 return matchedDrivers
 }
 
-fuzzyMatch = (driversArray, driversString) => {
+function fuzzyMatch (driversArray, driversString) {
   const matchedDrivers = driversArray.filter(drivers => {
     if (drivers.startsWith(driversString)){
       return true
@@ -18,7 +18,7 @@ fuzzyMatch = (driversArray, driversString) => {
 return matchedDrivers 
 }
 
-matchName = (driversArray, driversName) => {
+function matchName (driversArray, driversName) {
   const matchedDrivers = driversArray.filter(drivers => {
     if (driversName === drivers.name) {
       return true
