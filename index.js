47c257a10f81029cdfb2e38197e59fdba563790b
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-31 01:52:04 -0700

    Completed Lab; practiced using .filter() method to match and pull data from an array

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+findMatching = (driversArray, driversString) => {
+  const matchedDrivers = driversArray.filter(drivers => {
+    if (drivers.toLowerCase() === driversString.toLowerCase()) {
+      return true
+    } else {
+      return false
+}});
+return matchedDrivers
+}
+
+fuzzyMatch = (driversArray, driversString) => {
+  const matchedDrivers = driversArray.filter(drivers => {
+    if (drivers.startsWith(driversString)){
+      return true
+    } else {
+      return false
+}});
+return matchedDrivers 
+}
+
+matchName = (driversArray, driversName) => {
+  const matchedDrivers = driversArray.filter(drivers => {
+    if (driversName === drivers.name) {
+      return true
+    } else {
+      return false
+}});
+return matchedDrivers  
+}
